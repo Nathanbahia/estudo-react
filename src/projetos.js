d05a0a4c14e2419683ebad92adad61140b321ab5
nathanbahia <nathanbabahia@gmail.com>
2020-10-15 15:37:27 -0300

    Adiciona novos projetos

--- src/projetos.js
+++ src/projetos.js
@@ -17,6 +17,42 @@ export default function() {
             "texto": "Projeto de estudo sobre raspagem de dados e leitura de arquivos JSON usando Python para o webscraping e criação da interface usando JavaScript.",
             "imagem": "https://previews.dropbox.com/p/thumb/AA_L9oaD_LHoZzWBHFyO1Kj0Mb7PKzAQHV08Gm-HGEo2T4IbUI9paPbIxyzQ3ZJkbLoRng9KkSxig_0ZOx1m4vfoThHKC2wWWzKIH_y_9HFEUdScL3kOebel41tIFOzv1UnUFW_y1phwOl4MD3ZPGXCTHIxZjnof4HrFX_TZk6MsA_JplgBjtlLhNHrybV0JsHaFITvIihujOrRTl-nspWnZp4NvoPk3NpM1yJcfUc1qwR2H6MbGGXLoF2oTqEHfl5-f0Omt9lWSyC7v89b3ud1BGGGoY3552k_j_cBfbTmCK0PHS4gMSlqSAdQgnDkki5BUmwqrtFGPZZlhXQBEFg4AtZa5l5uT2L8X_JvghBiw9w/p.png?fv_content=true&size_mode=5",
             "link": "https://google.com",
-        },        
+        },
+        {
+            "titulo": "Bootstrap4 -3",
+            "texto": "Projeto de estudo desenvolvido durante o curso de JQuery + Bootstrap4 realizado na plataforma Udemy.",
+            "imagem": "https://previews.dropbox.com/p/thumb/AA8qMiy7_602ezJg6rSlqP1NIr9255gf1JRdCVbUftagOmfgCpXgo2DlGjACiCpViIeyss_s2QfozyDY29UGd3evxaODG-ibIu0KWsnugqp8v4ticTCq_z-m-T4s2Fssl_Yi8WFF5OYOgtrpQ42phtPaAf7ihTVchBtMs5mc4iz7dpWzGFqyfepFtA2-TtM3RDl7V-6vj99qlNitQN0CC-xnjgMaay_dmGnye2kS4V_qx1ykq8JCOIukk0e5LIOrLffPOokNmEXfbUVcOMT2zDOK3s8HMrXN6LJA8kiT_H7pKqts8wIxbBc-vSbTUePiV_i-5kvJiSyuEzMLAH5sOaCl11hBzJ9ig8sztXHOC5ILYw/p.png?fv_content=true&size_mode=5,
+            "link": "https://google.com",
+        },  
+        {
+            "titulo": "Bootstrap4 - 2",
+            "texto": "Projeto de estudo desenvolvido durante o curso de JQuery + Bootstrap4 realizado na plataforma Udemy.",
+            "imagem": "https://previews.dropbox.com/p/thumb/AA_V_fgzUXtU9L1S-MzKzpS_1CEM2CwO6zgZRz931OhgCpg6pjT8qmjCQoGhJDRa0d68y-kIqn03e4IndPwuFfNHGbBnPqSOlQ1HtUtaYkreUEM9bPghu7OinHLBepKExlDeDpIXwOQHE2ZscOxiN5Jp99SgI1dz8wz5RBP_Tq96xknYjuhk5LSSIyr5Bby8pdYgtnL4iJi4wZfhH6LY628OZZ-1KMqR_WFJB6eQaGcSXpO19b1RA-3TrqeiGJ6Ui0xBSntg5X93JH5__72jMoB7DMAXW6jBNTVLk_7rrM_otQzHsgB90dh1zb-JZDjhyMuhicCT8WWbm8YEFWoP2z5_Ff4ehR7b6rquSW71ADAYIQ/p.png?size=1600x1200&size_mode=3",
+            "link": "https://google.com",
+        },  
+        {
+            "titulo": "Bootstrap4  -1",
+            "texto": "Projeto de estudo desenvolvido durante o curso de JQuery + Bootstrap4 realizado na plataforma Udemy.",
+            "imagem": "https://previews.dropbox.com/p/thumb/AA82ZyQ783xrH1wVoqnM3k4dTEinM5eR5PL6wjrQmS_ucyGgW3DGiExUMh5Fkq3wNVNBFUmLxkqMKVMDj4QROsAvBSnvhE1EvOdCUt9r8cQPk32LvqX8zueGIrVqrgvxBfYyMB_CpTu6X13LJF3o1rpnJiVIC3YXgXsreFA_3KOGKvxkUT9ZVwUhyL8hF67o4ylY65I48q4g1tjXXTS0DSbWFlW_A09QQ7RZNw1KJtaijR3D1ZSEiBy01tBZcUtqPHUQBngnb0mAQpjaoJdWWcCZk6CzoWoGZW_uG3eXtQbS98TJd6LaK9l1WMyfBGQsUlwWR1ZVUYAugVMdtDMvVsdCSmGpGrTAi6pSYLdSl3rokA/p.png?fv_content=true&size_mode=5",
+            "link": "https://google.com",
+        },  
+        {
+            "titulo": "Consumo de Energia",
+            "texto": "Projeto desenvolvido em parceria com um colega de engenharia elétrica como trabalho acadêmico.",
+            "imagem": "https://previews.dropbox.com/p/thumb/AA86jH1IPDYt3cSwblBRNgUjwIO7ihn_ow62EnwK9lwBqtUq3HRHiEj8LHgCJgHWEVi4mgXmSalvuWZoPLjcTOK7zHyE4sMLXO-dRi-goqV22V0LmWwWuqkNmgLDEk2vxEru7tH5Vv_00hrf44NG3jH_14Mz50D2FpUkZmsTHR65qPtX56X4HMTevMx-O7n6nBPE6ycMm_w9pFWWvUHc7PrO-bgkqtd-eti-LuB_Usi4wMSK7crocaZuD0oluVIrI04xFRDazKnb2i5zO4vLtikXLLYi2fSnsd02OuoaCyvY6LZIjXP9ReY2t7nKuiTKMztZT5GT53LRlTT-H0MX_e93pHyDSMjGOYmL3NOu0jCgVA/p.png?fv_content=true&size_mode=5",
+            "link": "https://google.com",
+        },  
+        {
+            "titulo": "Dinheiro Agora",
+            "texto": "Estudo sobre requisições assincronas usando Axios e Javascript. Foi usado a ferramenta Google Charts para desenvolvimento dos gráficos da aplicação.",
+            "imagem": "https://previews.dropbox.com/p/thumb/AA-nLIwor6yHrxk2M-2Ly_Frk3OQ2SIDDBh26QNupOoBtKzXOZU57U-sP9dBZupOYhLNudJqUxor60SRBFjgoAJsD7MTenb5SRzuoMIr2EqpWpXsVHdiryo3ZBR8rUNzQUWjBTKWe0945nNBnsnuZcrIp_W4EX3ymz15Rz9V__wiyGHqObHxUCgzpRKwV5DAMayOmWEkRgWQTUtOGAAT-2FChCgylxLJsudXT-9c4qZMLC6_FuL-O37hqOrMq4YYSoH0Fp7ErY1a1GqNpFoCB_bgVG_9FSIHFXjuseDY-a4rdbKaJukBG-1Ewgj6WGx5aUP5Fsr1gY1DN-gWR3qXG_4sIohQdCmmicDN5g9QFILWSA/p.png?fv_content=true&size_mode=5",
+            "link": "https://google.com",
+        },  
+        {
+            "titulo": "Faça sua Parte",
+            "texto": "Projeto pessoal desenvolvido com o objetivo de compartilhar cursos gratuitos com certificado.",
+            "imagem": "https://previews.dropbox.com/p/thumb/AA_6jUuKkM3W9twWTLPVzpOfdT-LAWRuNq7fkxtCrC9ojrgQcb8kEn_2lc0lTh5_c-pmVZadx1A0PpELQkrAUqAMXd6E6YMC95ZgP24ws7vWjykLGAREmGkCBUZODIznDU2iVNy2ZcomsYzK6DcdsOuGxF1ZGZEPZrniOypLZ9m_FzkDF8GRufdr9DpA0N_qoZfd_D7o9HfGs8bD3XYW6oXsECOxUHe1eLePWloweLPe-v5kLXPHsbJXMu1w13yzK8mokUqvtsRm2fIJfXlJAuMrhnFcDddTjkLxN2xYLRE6fhQYimP2_Jyti68lrVezFlexlmA_naEWOefsSddMYyHFJtRsddWCmrQIlUss_0ikAg/p.png?fv_content=true&size_mode=5",
+            "link": "https://google.com",
+        },       
     ]
 }
