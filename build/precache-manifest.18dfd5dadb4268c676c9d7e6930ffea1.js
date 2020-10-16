020116b9f777549d739f7fb1e098eafac09b6cbf
nathanbahia <nathanbabahia@gmail.com>
2020-10-16 08:57:56 -0300

    Modifica estrutura de template e copia imagens

---
+++ build/precache-manifest.18dfd5dadb4268c676c9d7e6930ffea1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aea6315a2618a8f3a6e0e0aa749e606d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f37746947426b43a0c63",
+    "url": "/static/css/main.ccbfd438.chunk.css"
+  },
+  {
+    "revision": "88ff7c69a4a77dffb726",
+    "url": "/static/js/2.c367990e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c367990e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f37746947426b43a0c63",
+    "url": "/static/js/main.204def01.chunk.js"
+  },
+  {
+    "revision": "fbaab9a582e8e68a6c8c",
+    "url": "/static/js/runtime-main.2ae115fb.js"
+  }
+]);
--- src/index.js
+++ src/index.js
@@ -11,29 +11,28 @@ import Footer from './components/basic/Footer'
 import proj from './projetos'
 
 const projetos = proj();
+const projetoComponent = projetos.map( (projeto, i) => {
+    return (
+        <Projeto key= {i}
+            titulo={projeto.titulo} 
+            texto={projeto.texto} 
+            imagem={projeto.imagem} 
+            link={projeto.link}
+        />
+    )
+})  
 
 ReactDOM.render(
-    <Menu />,
-    document.querySelector("#menu")
-)
-ReactDOM.render(
-    <Banner />,
-    document.querySelector("#banner")
-)
-ReactDOM.render(
-    <div>
-        <div className="lista-projetos">            
-            <Projeto titulo={projetos[0].titulo} texto={projetos[0].texto} imagem={projetos[0].imagem} link={projetos[0].link}/>                                
-            <Projeto titulo={projetos[1].titulo} texto={projetos[1].texto} imagem={projetos[1].imagem} link={projetos[1].link}/>  
-            <Projeto titulo={projetos[2].titulo} texto={projetos[2].texto} imagem={projetos[2].imagem} link={projetos[2].link}/>  
-            <Projeto titulo={projetos[3].titulo} texto={projetos[3].texto} imagem={projetos[3].imagem} link={projetos[3].link}/>                                
-            <Projeto titulo={projetos[4].titulo} texto={projetos[4].texto} imagem={projetos[4].imagem} link={projetos[4].link}/>  
-            <Projeto titulo={projetos[5].titulo} texto={projetos[5].texto} imagem={projetos[5].imagem} link={projetos[5].link}/>  
-            <Projeto titulo={projetos[6].titulo} texto={projetos[6].texto} imagem={projetos[6].imagem} link={projetos[6].link}/>                                
-            <Projeto titulo={projetos[7].titulo} texto={projetos[7].texto} imagem={projetos[7].imagem} link={projetos[7].link}/>  
-            <Projeto titulo={projetos[8].titulo} texto={projetos[8].texto} imagem={projetos[8].imagem} link={projetos[8].link}/>  
+    <>
+        <Menu />
+
+        <Banner />
+
+        <div className="lista-projetos">          
+            { projetoComponent }
         </div>
+        
         <Footer title="Nathan Bahia" text="Obrigado por visitar!"/>
-    </div>,
+    </>,
     document.querySelector("#root")
 )
